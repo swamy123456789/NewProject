fdad34b4bd7bd74ef9b6f8b65092bf8aba7ed8e4
Parmeshwarrit <Parmeshwar.nagapalle@revappayyaitservices.com>
2026-01-08 11:32:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+// console.log("hello world")
+
+// // let name="param"
+// // console.log(name)
+
+// // name="omkar"
+// // console.log(name)
+
+// // const name2="diapk"
+// // console.log(name2)
+
+// // name2="abhi"
+// // console.log(name2)
+
+// let name="param"
+// console.log(name)
+
+// name="omkar"
+// console.log(name)
+
+// const name1="dipak"
+// console.log(name1)
+
+// name2="abhi"
+// console.log(name2)
+
+// var name3="car"
+// console.log(name3)
+
+// name3="bike"
+// console.log(name3)
+// console.log(name3.length)
+
+// var name3="new_car"
+// console.log(name3)
+// console.log(name3.length)
+
+
+
+// let name="hello"
+// console.log(name)
+
+
+// a=a-1
+var a=3
+var b=3 
+console.log(a<=b)  
+console.log(a>=b)  
+
+ 
+
